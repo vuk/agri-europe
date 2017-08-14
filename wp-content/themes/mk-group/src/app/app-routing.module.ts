595905fd597b34161822cdf8af73710cc5c67e57
Vuk Stanković <vuks89@gmail.com>
2017-08-14 23:19:19 +0200

    Added enter animation for home component

--- wp-content/themes/mk-group/src/app/app-routing.module.ts
+++ wp-content/themes/mk-group/src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
+import {MenuComponent} from "./menu/menu.component";
 
 const appRoutes: Routes = [
-    { path: '', component: HomeComponent }
+  {path: '', component: HomeComponent},
+  {path: 'menu', component: MenuComponent},
 ];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(appRoutes)
-    ],
-    exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
--- wp-content/themes/mk-group/src/app/home/home.component.ts
+++ wp-content/themes/mk-group/src/app/home/home.component.ts
@@ -1,26 +1,48 @@
 import {Component, OnInit} from '@angular/core';
 import {ConfigService} from "../services/config.service";
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('load', [
+      state('in', style ({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0
+        }),
+        animate(500)
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
-
-    config: Array<String>;
-    webmVideo: String = '';
-    mp4Video: String = '';
-    videoPoster: String = '';
-
-    constructor(private configService: ConfigService) {
-    }
-
-    ngOnInit() {
-        this.config = this.configService.getConfig();
-        this.webmVideo = this.config['home_webm'];
-        this.mp4Video = this.config['home_mp4'];
-        this.videoPoster = this.config['video_bg'];
-    }
-
+  
+  config: Array<String>;
+  webmVideo: String = '';
+  mp4Video: String = '';
+  videoPoster: String = '';
+  state: String = 'inactive';
+  
+  constructor(private configService: ConfigService) {
+  }
+  
+  ngOnInit() {
+    this.config = this.configService.getConfig();
+    this.webmVideo = this.config['home_webm'];
+    this.mp4Video = this.config['home_mp4'];
+    this.videoPoster = this.config['video_bg'];
+  }
+  
 }
